21cc2f26393991b8bdddb60ca8a728d7380f47d0
MHYNW <phdcarrot@naver.com>
2020-12-04 21:45:16 +0900

    add submodule

--- ACO.py
+++ ACO.py
@@ -1,17 +1,29 @@
 """
+
 Ant Colony Optimization for UAVs
 
 Author: MHYNW
-        https://github.com/MHYNW
-        phdcarrot@gmail.com
+        #https://github.com/MHYNW
+        #phdcarrot@gmail.com
 
 """
 
+import math
+from enum import Enum
 import numpy as np
+import ant_colony_optimization.ant_colony
+
 
 class ACO():
     def __init__(self):
         print("start ACO")
+        waypoints = []
+        tau = []
 
 
-    def
+    # def SystemModel():
+
+
+    # def Heuristic():
+
+
